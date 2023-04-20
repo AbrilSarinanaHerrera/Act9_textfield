8e5065597bf92d3d664611367e01638b58f141b4
User_T7 <a.20308051280553@cbtis128.edu.mx>
2023-04-20 22:22:31 +0000

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -12,31 +12,76 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Hello World',
       // Application theme data, you can set the colors for the application as
       // you want
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.pink,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Tabla Cliente'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+        appBar: AppBar(
+          // The title text which will be shown on the action bar
+          title: Text(title),
+          centerTitle: true,
         ),
-      ),
-    );
+        body: Container(
+            padding: EdgeInsets.all(20),
+            child: ListView(
+              children: [
+                Text("Ingresa tus datos:"),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Ingresa los datos",
+                      labelText: "Usuario",
+                      icon: Icon(Icons.account_circle)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Ingresa los datos",
+                      labelText: "Apellido",
+                      icon: Icon(Icons.account_circle)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Ingresa los datos",
+                      labelText: "Direccion",
+                      icon: Icon(Icons.location_on_rounded)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Ingresa los datos",
+                      labelText: "Flor",
+                      icon: Icon(Icons.loyalty)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Ingresa los datos",
+                      labelText: "Acompañante",
+                      icon: Icon(Icons.loyalty)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Ingresa los datos",
+                      labelText: "Cantidad",
+                      icon: Icon(Icons.loyalty)),
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                      hintText: "Ingresa los datos",
+                      labelText: "Papel",
+                      icon: Icon(Icons.loyalty)),
+                ),
+              ],
+            )));
   }
 }
